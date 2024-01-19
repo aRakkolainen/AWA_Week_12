e162f0d16df0291765dd672fc7dc676719248cad
aRakkolainen <aino.rakkolainen@gmail.com>
2024-01-19 12:57:33 +0200

    Tasks 1 and 2

---
+++ server/routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+const Books = require("../models/Books");
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+// Route for users to save books to database
+router.post("/api/book", async (req, res) => {
+  if (req.body) {
+    let newBook = new Books({name: req.body.name, author: req.body.author, pages: req.body.pages});
+    await newBook.save(); 
+    res.sendStatus(200);
+  } else {
+    res.send("No book to add!")
+  }
+})
+
+module.exports = router;
